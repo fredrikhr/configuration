bb668fa3bf87a185ec133322e239559caa7063bd
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-06-29 19:37:06 +0200

    Change dotnet-completion script to auto download from GitHub

--- PowerShell/ps1.d/dotnet-completion.ps1
+++ PowerShell/ps1.d/dotnet-completion.ps1
@@ -1,7 +1,22 @@
-# PowerShell parameter completion shim for the dotnet CLI 
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-     param($commandName, $wordToComplete, $cursorPosition)
-     dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-     }
- }
+& {
+    if (Get-Command "dotnet" -ErrorAction SilentlyContinue) {
+        & "dotnet" complete | Out-Null
+        if ($LASTEXITCODE -eq 0) {
+            $local:scriptblock = {
+                param($wordToComplete, $commandAst, $cursorPosition)
+                dotnet complete --position $cursorPosition $commandAst.ToString() | ForEach-Object {
+                    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+                }
+            }
+            Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $local:scriptblock
+        } else {
+            $LASTEXITCODE = 0
+        }
+    }
+}
+
+& {
+    if (Get-Command "dotnet-suggest" -ErrorAction SilentlyContinue) {
+        & ([scriptblock]::Create((& "dotnet-suggest" script PowerShell | Out-String)))
+    }
+}
