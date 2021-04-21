841b05dfee2f152609ebeb5e6e43ba26fc5dfd4f
Fredrik Høisæther Rasch <fredrik.rasch@arva.no>
2021-04-21 15:07:06 +0200

    Speed up powershell completion-scripts

--- PowerShell/ps1.d/dotnet-completion.ps1
+++ PowerShell/ps1.d/dotnet-completion.ps1
@@ -1,22 +1,8 @@
-& {
-    if (Get-Command "dotnet" -ErrorAction SilentlyContinue) {
-        & "dotnet" complete | Out-Null
-        if ($LASTEXITCODE -eq 0) {
-            $local:scriptblock = {
-                param($wordToComplete, $commandAst, $cursorPosition)
-                dotnet complete --position $cursorPosition $commandAst.ToString() | ForEach-Object {
-                    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-                }
-            }
-            Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $local:scriptblock
-        } else {
-            $LASTEXITCODE = 0
-        }
-    }
+$shimFilePath = Join-Path $PSScriptRoot "dotnet-completion-script.ps1"
+if (Test-Path $shimFilePath -NewerThan ([datetime]::Today.Subtract([timespan]::FromDays(7)))) {
+    return
 }
 
-& {
-    if (Get-Command "dotnet-suggest" -ErrorAction SilentlyContinue) {
-        & ([scriptblock]::Create((& "dotnet-suggest" script PowerShell | Out-String)))
-    }
+if (Get-Command "dotnet-suggest" -ErrorAction SilentlyContinue) {
+    & ([scriptblock]::Create((& dotnet-suggest script PowerShell | Tee-Object -FilePath $shimFilePath)))
 }
--- PowerShell/ps1.d/github-completion.ps1
+++ PowerShell/ps1.d/github-completion.ps1
@@ -1,3 +1,8 @@
+$ghCompletionFilePath = Join-Path $PSScriptRoot "github-completion-script.ps1"
+if (Test-Path $ghCompletionFilePath -NewerThan ([datetime]::Today.Subtract([timespan]::FromDays(7)))) {
+    return
+}
+
 if (Get-Command "gh" -ErrorAction SilentlyContinue) {
-    & ([scriptblock]::Create((& "gh" completion -s powershell | Out-String)))
+    & ([scriptblock]::Create((& "gh" completion -s powershell | Tee-Object -FilePath $ghCompletionFilePath)))
 }
