d5d1ee295147536a8bbb33009690f3611b3af5f9
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2023-06-26 11:04:44 +0200

    PowerShell: replace [string]::Join with -join operator

--- PowerShell/ps1.d/prompt.ps1
+++ PowerShell/ps1.d/prompt.ps1
@@ -1,6 +1,5 @@
 function prompt {
-    [string]::Join([System.Environment]::NewLine,
-        "PS $($ExecutionContext.SessionState.Path.CurrentLocation)",
-        "$('>' * ($nestedPromptLevel + 1)) "
-    )
+    "PS $($ExecutionContext.SessionState.Path.CurrentLocation)",
+    "$('>' * ($nestedPromptLevel + 1)) " `
+        -join [System.Environment]::NewLine
 }
